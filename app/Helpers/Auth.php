1a85b1a34fedcb469cdded7fd79d332b60a4cec3
seanyboyswag <2150218@slu.edu.ph>
2020-04-27 16:01:50 +0800

    Refactored Auth helper class

--- app/Helpers/Auth.php
+++ app/Helpers/Auth.php
@@ -2,14 +2,11 @@
 
 namespace App\Helpers;
 
-use Config\Database;
-
 class Auth
 {
-
     public static function login($user)
     {
-        session()->set('email', $user->email);
+        session()->set('user', serialize($user));
     }
 
     public static function logout()
@@ -19,15 +16,11 @@ class Auth
 
     public static function user()
     {
-        $email = session()->get('email');
+        $user = unserialize(session()->get('user'));
 
-        if (!$email) {
+        if (!$user) {
             return null;
         } else {
-            $db = Database::connect();
-            $queryStatement = "SELECT * FROM users u WHERE u.email = '{$email}'";
-            $user = $db->query($queryStatement)->getRow();
-
             return $user;
         }
     }
